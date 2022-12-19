19fa8f1ca52e6c985cd2572915fafda4aa8ce596
Bill Blanchette <wlblanchette@gmail.com>
2022-12-18 23:31:57 -0500

    fix none type failure

--- polyjuice_common/sns.py
+++ polyjuice_common/sns.py
@@ -8,6 +8,10 @@ def unpack_sns_message(fn: Callable):
   Unpacks an Sns event's message adds it as a kwarg
   """
   def inner(*args, **kwargs):
-    sns_payload = json.loads(safe_access(args[0], ['Records', 0, 'Sns', 'Message']))
-    return fn(*args, **kwargs, sns_payload=sns_payload)
+    payload = safe_access(args[0], ['Records', 0, 'Sns', 'Message'])
+    if payload:
+      sns_payload = json.loads(payload)
+      return fn(*args, **kwargs, sns_payload=sns_payload)
+    print('[unpack_sns_message] Not an Sns payload')
+    return None
   return inner
